c5422a2187f18023c00e7fe37af3c9fc9658d21f
otofixcenter <otofixcenter@gmail.com>
2025-08-04 09:36:52 +0300

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import google.generativeai as genai
 import os
 
 # Get the API key from environment variables
-api_key = os.environ.get("GOOGLE_API_KEY")
+api_key = os.environ.get("API_KEY")
 
 if not api_key:
     st.error("API Key not found. Please set the GOOGLE_API_KEY environment variable.")
